89bb0bd592f9f8a30a60daf528f4520382fdf15f
James Patrick Oentoro <joentoro41@students.calvin.ac.id>
2023-12-06 16:02:17 +0700

    modify student login

--- flask-app/app.py
+++ flask-app/app.py
@@ -37,15 +37,37 @@ def login_student():
         data = request.get_json()
         # print(data)
         # print(data['username'])
-        curr_username  = data['name']
+        id_jemaat  = data['id_jemaat']
         login_time = datetime.now()
-        return jsonify(add_absen(curr_username,login_time))
-    return
+        return jsonify(add_absen(id_jemaat,login_time))
+    return jsonify({'status':'failed'})
+
+@app.route('/regristration',methods=['POST','GET'])
+def student_regristration():
+    if request.method == 'POST':
+        data = request.get_json()
+        nama = data['nama']
+        no_telp = data['no_telp']
+        email = data['email']
+        gender = data['gender']
+        hobi = data['hobi']
+        sekolah = data['sekolah']
+        temp_lahir = data['temp_lahir']
+        tgl_lahir = data['tgl_lahir']
+        no_telp_ortu = data['no_telp_ortu']
+        kelas = data['kelas']
+        daerah = data['daerah']
+        kecamatan = data['kecamatan']
+        alamat = data['alamat']
+        foto = data['foto']
+        status = data['status']
+        return jsonify(add_jemaat(nama, no_telp, email, gender, hobi, sekolah, temp_lahir, tgl_lahir, no_telp_ortu, kelas, daerah, kecamatan, alamat, foto, status))
+    return jsonify({'status':'failed'})
 
 if __name__ == '__main__':
     # initiate_table()
     # hostname = socket.gethostname()
     # ip_address = socket.gethostbyname(hostname)
-    app.run( port=5000, debug=True)
+    app.run(port=5000, debug=True)
 
 
